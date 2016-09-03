5652a5f42ab66e5ab9259f584f74f0f25f60685f
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-04 00:38:33 +0300

    implement find_sum

--- 01/src/ouvain_t01.erl
+++ 01/src/ouvain_t01.erl
@@ -4,6 +4,13 @@
          find_sum/2
         ]).
 
-find_sum(Multipliers, Max) ->
-    erlang:error(not_implemented).
+find_sum(Multipliers, Max) when is_integer(Max), Max > 0 ->
+    L = [X || X <- lists:seq(1, Max - 1), is_suitable(X, Multipliers)],
+    lists:sum(L).
+
+is_suitable(N, Multipliers) ->
+    F = fun(Multiplier) ->
+                (N rem Multiplier) =:= 0
+        end,
+    lists:any(F, Multipliers).
 
