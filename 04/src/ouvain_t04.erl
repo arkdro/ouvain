d762f1d171750121f1557ffa77d4f79917690620
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-07 10:32:31 +0300

    find_divisor

--- 04/src/ouvain_t04.erl
+++ 04/src/ouvain_t04.erl
@@ -7,3 +7,11 @@
 t() ->
     true.
 
+find_divisor(N) ->
+    find_divisor(N, 1).
+
+find_divisor(_, Divisor) when N < 10 ->
+    Divisor;
+find_divisor(N, Divisor) ->
+    find_divisor(N div 10, Divisor * 10).
+
