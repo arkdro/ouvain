b35df44ce8a2df27c18682cbf474087aa0b7dcd0
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-07 10:32:47 +0300

    is_palindrome

--- 04/src/ouvain_t04.erl
+++ 04/src/ouvain_t04.erl
@@ -7,6 +7,20 @@
 t() ->
     true.
 
+is_palindrome(N, _Divisor) when N >= 0, N < 10 ->
+    true;
+is_palindrome(N, Divisor) ->
+    Beg = N div Divisor,
+    End = N rem 10,
+    case Beg =:= End of
+        true ->
+            N2 = N div 10,
+            Divisor2 = Divisor div 10,
+            is_palindrome(N2, Divisor2);
+        false ->
+            false
+    end.
+
 find_divisor(N) ->
     find_divisor(N, 1).
 
