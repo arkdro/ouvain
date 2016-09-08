81549d4329eb1fb6ed8152cb4cc8ed21457d5fcc
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-08 10:13:34 +0300

    is_palindrom

--- 04/src/ouvain_t04.erl
+++ 04/src/ouvain_t04.erl
@@ -1,40 +1,20 @@
 -module(ouvain_t04).
 
 -export([
-         t/0
+         is_palindrome/1
         ]).
 
-t() ->
-    true.
+is_palindrome(N) ->
+    is_palindrome(N, []).
 
-find(N) ->
-    Divisor = find_divisor(N),
-    case is_palindrome(N, Divisor) of
-        true ->
-            N;
-        false ->
-            find()
-    end.
+is_palindrome(N, Acc) when N < 10 ->
+    compare([N | Acc]);
+is_palindrome(N, Acc) ->
+    Digit = N rem 10,
+    N2 = N div 10,
+    is_palindrome(N2, [Digit | Acc]).
 
-is_palindrome(N, _Divisor) when N >= 0, N < 10 ->
-    true;
-is_palindrome(N, Divisor) ->
-    Beg = N div Divisor,
-    End = N rem 10,
-    case Beg =:= End of
-        true ->
-            N2 = N div 10,
-            Divisor2 = Divisor div 10,
-            is_palindrome(N2, Divisor2);
-        false ->
-            false
-    end.
-
-find_divisor(N) ->
-    find_divisor(N, 1).
-
-find_divisor(_, Divisor) when N < 10 ->
-    Divisor;
-find_divisor(N, Divisor) ->
-    find_divisor(N div 10, Divisor * 10).
+compare(Digits) ->
+    Normal = lists:reverse(Digits),
+    Normal =:= Digits.
 
