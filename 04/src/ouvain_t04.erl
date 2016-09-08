fd8127776adce43e41ec1f4293cb299508993bff
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-08 10:14:01 +0300

    add tests

--- 04/src/ouvain_t04.erl
+++ 04/src/ouvain_t04.erl
@@ -18,3 +18,23 @@ compare(Digits) ->
     Normal = lists:reverse(Digits),
     Normal =:= Digits.
 
+%% ------------------------------------------------------------------
+%% eunit
+%% ------------------------------------------------------------------
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+is_palindrom_test() ->
+    ?assertMatch(true, compare([1,2,3,4,3,2,1])),
+    ?assertMatch(true, compare([1,2,3,4,4,3,2,1])),
+    ?assertMatch(true, compare([4])),
+    ?assertMatch(true, compare([4,4])),
+    ?assertMatch(true, compare([1,1])),
+    ?assertMatch(false, compare([1,2,3,4,4,3,2,1,0])),
+    ?assertMatch(false, compare([1,0])),
+    ?assertMatch(false, compare([2,1])),
+    ok.
+
+-endif.
+
--- 04/test/t04_SUITE.erl
+++ 04/test/t04_SUITE.erl
@@ -23,7 +23,7 @@ groups() ->
                 {group, read}
                ]},
      {read, [], [
-                 t1
+                 test_palindrome
                 ]}
     ].
 
@@ -33,7 +33,14 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
     ok.
 
-t1(_Config) ->
-    ?assertMatch(true, ouvain_t04:t()),
+test_palindrome(_Config) ->
+    ?assertMatch(true, ouvain_t04:is_palindrome(1234321)),
+    ?assertMatch(true, ouvain_t04:is_palindrome(12344321)),
+    ?assertMatch(true, ouvain_t04:is_palindrome(4)),
+    ?assertMatch(true, ouvain_t04:is_palindrome(44)),
+    ?assertMatch(true, ouvain_t04:is_palindrome(11)),
+    ?assertMatch(false, ouvain_t04:is_palindrome(123443210)),
+    ?assertMatch(false, ouvain_t04:is_palindrome(10)),
+    ?assertMatch(false, ouvain_t04:is_palindrome(21)),
     ok.
 
