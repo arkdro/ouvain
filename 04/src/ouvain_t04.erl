78e29ff1a04148e608d03eddde4d868cc6c416ba
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-11 01:42:08 +0300

    adding find2

--- 04/src/ouvain_t04.erl
+++ 04/src/ouvain_t04.erl
@@ -2,6 +2,7 @@
 
 -export([
          find/1,
+         find2/1,
          is_palindrome/1
         ]).
 
@@ -11,6 +12,77 @@ find(Len) ->
                               N2 <- lists:seq(Start, 1, -1),
                               is_palindrome(N1 * N2)],
     hd(lists:reverse(lists:sort(L))).
+
+find2(Len) ->
+    Max = get_start(Len),
+    Params = params(),
+    {Digit, Deltas} = hd(Params),
+    Min = build_min(Digit, Len),
+    N1 = Max,
+    N2 = Max,
+    X = N1 * N2,
+    Acc = build_nums(X, N1, N2, Min, Max, Max, Deltas, []),
+    case has_result(Acc) of
+        true ->
+            stub;
+        false ->
+            find(Len)
+    end.
+
+has_result([]) ->
+    false;
+has_result([_|_]) ->
+    true.
+
+%% only for 9xx
+build_nums(_X, _N1, _N2, _Min, _Max1, _Max2, [], Acc) ->
+    Acc;
+build_nums(X, N1, N2, Min, Max1, Max2, [{D1, D2} | T] = Deltas, Acc) ->
+    case too_small(X, Min) of
+        true ->
+            N1_2 = Max1,
+            N2_2 = N2 - D2,
+            X2 = N1_2 * N2_2,
+            case too_small(X2, Min) of
+                true ->
+                    build_nums(Max1 * Max2, Min, Max1, Max2, Max1, Max2, T, Acc);
+                false ->
+                    X2 = N1_2 * N2_2,
+                    build_nums(X2, N1_2, N2_2, Min, Max1, Max2, Deltas, Acc)
+            end;
+        false ->
+            Acc2 = add_if_palindrome(X, Acc),
+            N1_2 = N1 - D1,
+            X2 = N1_2 * N2,
+            build_nums(X2, N1_2, N2, Min, Max1, Max2, Deltas, Acc2)
+    end.
+
+build_min(Digit, Len) ->
+    round(Digit * math:pow(10, 2 * Len - 1)).
+
+add_if_palindrome(X, Acc) ->
+    case is_palindrome(X) of
+        true ->
+            [X | Acc];
+        false ->
+            Acc
+    end.
+
+too_small(N, Min) ->
+    N < Min.
+
+params() ->
+    [
+     {9, [{1,9}, {3,3}]},
+     {8, [{1,8}, {2,4}]},
+     {7, [{1,7}]},
+     {6, [{1,6}, {2,3}]},
+     {5, [{1,5}]},
+     {4, [{1,4}, {2,2}]},
+     {3, [{1,3}]},
+     {2, [{1,2}]},
+     {1, [{1,1}]}
+    ].
 
 get_start(Len) ->
     round(math:pow(10, Len)) - 1.
