c2aa87f95baecf174ddf2749a01ebfebd4ddacaf
Arkadiy Drovosekov <arkdro@gmail.com>
2016-09-23 18:59:46 +0300

    init t07

---
+++ 07/apps/t07/src/t07_app.erl
@@ -0,0 +1,16 @@
+-module(t07_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    t07_sup:start_link().
+
+stop(_State) ->
+    ok.
