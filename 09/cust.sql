48f4bcc3807b873d2d086ad18cecf5f64e9e5ae0
Arkadiy Drovosekov <arkdro@gmail.com>
2016-10-01 23:05:59 +0300

    fix sql script

--- 09/cust.sql
+++ 09/cust.sql
@@ -1,16 +1,17 @@
 delimiter //
 drop table if exists assignment;
+drop table if exists product;
+
+create table product (id int primary key,
+       current_owner_id int)//
+
 create table assignment (id int primary key,
        product_id int,
        participant_id int,
        ts timestamp,
        action char(1),
-       foreign key (product_id) references product(id) on delete cascade,
+       foreign key (product_id) references product(id) on delete cascade
        )//
-
-drop table if exists product;
-create table product (id int primary key,
-       current_owner_id int)//
 
 drop procedure if exists get_chain_of_custody;
 create procedure get_chain_of_custody(in product_id int)
